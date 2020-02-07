0fb82e203d3fefc9274f9049c02f06de7947d5b7
Olatokunbo Ogunlade <Olatokunbo.Ogunlade@VentureGardenGroup.Local>
2020-02-07 15:22:21 +0100

    solved the key issue

--- src/Components/ListScreen.js
+++ src/Components/ListScreen.js
@@ -11,11 +11,12 @@ const ListScreen = () => {
         { name: 'Friend #6'},
         { name: 'Friend #7'},
         { name: 'Friend #8'},
-        { name: 'Friend #9'},
+        { name: 'Friend #9'}
         
     ];
     return (
         <FlatList
+        keyExtractor={friend => friend.name}
         data={friends}
         renderItem={({ item }) => {
             return <Text>{item.name}</Text>
