78fbed9585a12ada9b618919ade690a67d8ca345
Olatokunbo Ogunlade <Olatokunbo.Ogunlade@VentureGardenGroup.Local>
2020-03-13 15:24:09 +0100

    more examples on passing props

--- src/Components/ImageScreen.js
+++ src/Components/ImageScreen.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import {View, Text,StyleSheet,Button} from 'react-native';
-import ImageDetails from '../ComponentsAgain/ImageDetails';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import ImageDetails from "../ComponentsAgain/ImageDetails";
 
-
-function ImageScreen (){
-    return(
-      <View>
-         <ImageDetails title="forest"/>
-         <ImageDetails title="beach"/>
-         <ImageDetails title="beachhouse"/>
-         <ImageDetails title="beach2"/>
-      </View>
-    )
-};
+function ImageScreen() {
+  return (
+    <View>
+      <ImageDetails
+        title="forest"
+        imageSource={require("../../assets/forest.jpg")}
+        Score={7}
+      />
+      <ImageDetails
+        title="beach"
+        imageSource={require("../../assets/beach.jpg")}
+        Score={4}
+      />
+      <ImageDetails
+        title="mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+        Score={5}
+      />
+    </View>
+  );
+}
 
 export default ImageScreen;
--- src/ComponentsAgain/ImageDetails.js
+++ src/ComponentsAgain/ImageDetails.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import {View, Text,StyleSheet,Button} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
-
-const ImageDetails= props =>{
-  console.log(props);
-    return(
-      <View>
-         <Text>{props.title}</Text>
-      </View>
-    )
+const ImageDetails = ({title, imageSource,Score}) => {
+  // console.log(props);
+  return (
+    <View>
+      <Text>{title}</Text>
+      <Image source={imageSource} />
+      <Text>Image Score is - {Score}</Text>
+    </View>
+  );
 };
 
 export default ImageDetails;
